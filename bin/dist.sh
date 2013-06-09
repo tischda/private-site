46d1c74a312506e6ee926cf39bf1cf9a50674780
Daniel TISCHER <dos.7182@gmail.com>
2013-06-09 10:43:16 +0200

    alternative using find and xargs with batches of 10 and 3 parallel processes

--- bin/dist.sh
+++ bin/dist.sh
@@ -18,8 +18,13 @@ for f in */; do
   fi
 done
 
-$NOX_BIN/xbuild .xml "$NOX_BIN/nox --catalogs --nodtdattr -t $NOX_DIST -d menu.xi $* $NOX_HOME/xslt/common.xsl"
+# Using xbuild:
+#$NOX_BIN/xbuild .xml "$NOX_BIN/nox --catalogs --nodtdattr -t $NOX_DIST -d menu.xi $* $NOX_HOME/xslt/common.xsl"
+
+# Using find and xargs with batches of 10 and 3 parallel processes
+find * -name "*.xml" -print0 | xargs -0r -n 10 -P 3 $NOX_BIN/nox --catalogs --nodtdattr -t $NOX_DIST -d menu.xi $* $NOX_HOME/xslt/common.xsl
 
 echo -e "\nCopying resources to $NOX_DIST"
 rsync -a $NOX_HOME/res/ $NOX_DIST
 echo -e
+
